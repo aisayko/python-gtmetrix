dbc257b166b80cd94d99a7560ce99a82e2c1ede3
samycici <samycici@gmail.com>
2015-12-28 09:50:41 -0200

    include all sites

--- analysis.py
+++ analysis.py
@@ -7,10 +7,12 @@ with open('sites.json') as data_file:
     list_sites = json.load(data_file)
 
     for key, value in list_sites.items():
-        print key, value
+        print ("Site analisado: %s - Url: %s" % (key, value))
         gt = GTmetrixInterface('equipe-qualidade@infoglobo.com.br', '7207b7afe6497706233d1bcd71f8d891')
+        print ("Iniciando Análise")
         my_test = gt.start_test(value)
         time.sleep(60)
+        print ("Gravando resultados")
         results = gt.poll_state_request(key, my_test.test_id)
 
 
