ce608a513130fb66a362df7cdc48bbf1c2cbbc76
Steve Steiner <ssteinerX@gmail.com>
2017-03-25 16:02:12 -0400

    Add ‘IsNone’ tests to be more specific about why validation failed

--- gtmetrix/exceptions.py
+++ gtmetrix/exceptions.py
@@ -2,43 +2,63 @@
 Python GTmetrix API Library
 
 Exceptions
+
+TODO: ss -- rearrange __all__ and code to match and alphabetical order
 """
-__all__ = [
-            'GTmetrixInvalidTestRequest',
-            'GTmetrixBadAPIUrl',
-            'GTmetrixMissingAPIKey',
-            'GTmetrixMissingEmail',
-            'GTmetrixMissingEmailOrAPIKey',
-            'GTmetrixManyConcurrentRequests',
-            'GTmetrixMaximumNumberOfApis',
-            'GTmetrixTestNotFound',
-          ]
+__all__ = ['GTmetrixInvalidTestRequest',
+           'GTmetrixAPIUrlIsNone',
+           'GTmetrixBadAPIUrl',
+           'GTmetrixAPIKeyIsNone',
+           'GTmetrixEmailIsNone',
+           'GTmetrixInvalidEmail',
+           'GTmetrixEmailIsNotStringtype',
+           'GTmetrixMissingEmailOrAPIKey',
+           'GTmetrixManyConcurrentRequests',
+           'GTmetrixMaximumNumberOfApis',
+           'GTmetrixTestNotFound',
+           ]
 
 
 class GTmetrixBadAPIUrl(Exception):
-    """URL is not the same as what was in settings"""
+    """URL is not the same as what was in settings."""
     pass
 
 
-class GTmetrixMissingAPIKey(Exception):
-   """API key is not in settings or passed to initializer."""
-   def __init__(self, message=None):
-       self.message = (message or
-           "API key must be passed or exist in the settings.")
+class GTmetrixAPIUrlIsNone(Exception):
+    """The URL is `None`."""
+    pass
 
 
-class GTmetrixMissingEmail(Exception):
-    """Email is not in settings or passed to initializer."""
+class GTmetrixAPIKeyIsNone(Exception):
+    """API key is not in settings or passed to initializer."""
+
     def __init__(self, message=None):
         self.message = (message or
-            "Email must be passed or exist in the settings.")
+                        "API key must be passed or exist in the settings.")
+
+
+class GTmetrixEmailIsNone(Exception):
+    """Email is not in settings or passed to initializer."""
+
+    def __init__(self, message=None):
+        self.message = (message or
+                        "Email must be passed or exist in the settings.")
+
+
+class GTmetrixEmailIsNotStringtype(Exception):
+    """Email is not a string type."""
+
+    def __init__(self, message=None):
+        self.message = (message or
+                        "Email is not a stringtype.")
 
 
 class GTmetrixInvalidEmail(Exception):
     """Email is invalid."""
+
     def __init__(self, message=None):
         self.message = (message or
-            "Email is invalid.")
+                        "Email is invalid.")
 
 
 class GTmetrixMissingEmailOrAPIKey(Exception):
@@ -48,9 +68,10 @@ class GTmetrixMissingEmailOrAPIKey(Exception):
           used only to validate when invalid test data is produced while
           testing.
     """
+
     def __init__(self, message=None):
         self.message = (message or
-            "Email and API key must be passed or exist in the settings.")
+                        "Email and API key must be passed or exist in the settings.")
 
 
 class GTmetrixInvalidTestRequest(Exception):
